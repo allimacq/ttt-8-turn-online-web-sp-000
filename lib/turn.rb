44618d18014a80cf4a308b71799fd6cc8ef79ab6
Allison _ <allimacq@gmail.com>
2020-12-14 20:26:07 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,72 @@
+#defining the turn method using the helper methods defined below
+
+def turn(board)
+  #asking the user for their input
+  puts "Please enter 1-9:"
+  #the gets method actually *gets* the user input
+  input = gets
+  
+  #now calling the input_to_index method to convert the input to an integer and to the proper ruby index equivalent
+  index = input_to_index(input)
+  
+  #calling the move index
+  move(board, index)
+  
+  #now we need to validate the user's move
+  valid_move?(board, index)
+  
+  display_board(board)
+
+end
+
+# Define display_board that accepts a board and prints
+# out the current state.
+def display_board(board = [" "," "," "," "," "," "," "," "," "])
+  row_one = " #{board[0]} | #{board[1]} | #{board[2]} "
+  dashes = "-----------"
+  row_two = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row_three = " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts row_one
+  puts dashes
+  puts row_two
+  puts dashes
+  puts row_three
+end
+
+#defining the input_to_index method
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+
+#move method
+def move(board, index, character = "X")
+  board[index] = character
+end
+
+
+#valid_move? method 
+def valid_move?(board, index)
+  #this first if statement is checking if the index is on the board and if the position has been taken
+  if index.between?(0,8) && position_taken?(board, index) == false
+    true
+  elsif index > 9 || index < 0
+    puts "Please enter 1-9:"
+    input = gets.to_i
+    false
+  elsif position_taken?(board, index) == true
+    false
+  end
+end
+
+
+# defining #position_taken? method so that it can be used in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
